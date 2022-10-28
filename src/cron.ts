685b242c7320ed3a4e6b73b51e654ceb0ac76397
ixmorrow <ivanmorrow14@gmail.com>
2022-10-27 22:08:52 -0500

    fix /addContest

--- src/cron.ts
+++ src/cron.ts
@@ -1,7 +1,23 @@
-const cron = 'node-cron'
+import { Contest } from './db/connection'
+var cron = require('node-cron')
 
-//cron.schedule('* * * * * *', function, options)
+// exports.initScheduledJobs = () => {
+//     const task = cron.schedule('* * * * * *', fetchOpenContests())
+//     task.start()
+// }
 
-async function fetchOpenContests() {
+exports.initScheduledJobs = () => {
+    const scheduledJobFunction = cron.schedule("* * * * * *", () => {
+        fetchOpenContests()
+    })
 
+    scheduledJobFunction.start()
+}
+
+export async function fetchOpenContests() {
+    await Contest.sync()
+    const activeContests = await Contest.findAll()
+    //console.log(activeContests.every(contest => contest instanceof Contest))
+    console.log(activeContests)
+    console.log("All contests:", JSON.stringify(activeContests, null, 2))
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express'
 import { Contest } from './db/connection'
 import dotenv from 'dotenv'
+import { fetchOpenContests } from './cron'
 
+const scheduledFunctions = require('./cron')
 const bodyParser = require("body-parser")
 const app = express()
 dotenv.config()
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
+//scheduledFunctions.initScheduledJobs()
 
 app.get('/', (req, res) => {
     res.send({
@@ -17,7 +20,7 @@ app.get('/', (req, res) => {
 
 app.post('/addContest', (req, res) => {
     console.log(req.body)
-    const contest = Contest.build({ 
+    const contest = Contest.create({ 
         contestPubkey: req.body.contestPubkey,
         creator: req.body.creator,
         pythPriceFeed: req.body.priceFeed,
@@ -28,6 +31,13 @@ app.post('/addContest', (req, res) => {
 
     res.send({
         status: "success"
+    })
+})
+
+app.get('/fetchContests', (req, res) => {
+    fetchOpenContests()
+    res.send({
+        message: "contests fetched"
     })
 })
 
