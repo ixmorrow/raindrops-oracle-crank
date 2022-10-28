7539ea8263b1b8440aac9d0c1acb2c42dea745f6
ixmorrow <ivanmorrow14@gmail.com>
2022-10-28 09:51:12 -0500

    revoke test tx

--- src/cron.ts
+++ src/cron.ts
@@ -6,11 +6,11 @@ import { programAuthority } from './utils/util'
 var cron = require('node-cron')
 
 // const programId = new PublicKey("887fNBoewDudLs7GUdEBFfpMZ9EmcFREK9Gmr3KY6pS7")
-const cluster = clusterApiUrl("devnet")
-const connection = new Connection(cluster)
-const wallet = new anchor.Wallet(programAuthority)
-const provider = new anchor.AnchorProvider(connection, wallet, {commitment: "confirmed"})
-const program = anchor.workspace.EventsOracle as anchor.Program<EventsOracle>
+// const cluster = clusterApiUrl("devnet")
+// const connection = new Connection(cluster)
+// const wallet = new anchor.Wallet(programAuthority)
+//const provider = new anchor.AnchorProvider(connection, wallet, {commitment: "confirmed"})
+//const program = anchor.workspace.EventsOracle as anchor.Program<EventsOracle>
 
 exports.initScheduledJobs = () => {
     const scheduledJobFunction = cron.schedule("* * * * * *", () => {
@@ -26,23 +26,23 @@ export async function fetchOpenContests() {
     let activeContests = await Contest.findAll()
     //console.log(activeContests[0].dataValues.endTime)
 
-    for (let i = 0; i < activeContests.length; i++) {
-        if (activeContests[i].dataValues.endTime <= currentUnixTime) {
-            let contest = new PublicKey(activeContests[i].dataValues.contestPubkey)
-            let priceFeed = new PublicKey(activeContests[i].dataValues.pythPriceFeed)
+    // for (let i = 0; i < activeContests.length; i++) {
+    //     if (activeContests[i].dataValues.endTime <= currentUnixTime) {
+    //         let contest = new PublicKey(activeContests[i].dataValues.contestPubkey)
+    //         let priceFeed = new PublicKey(activeContests[i].dataValues.pythPriceFeed)
 
-            // close the contest
-            const tx = await program.methods.endEvent()
-            .accounts({
-                authority: programAuthority.publicKey,
-                event: contest,
-                pythPriceFeed: priceFeed
-            })
-            .signers([programAuthority])
-            .rpc()
+    //         // close the contest
+    //         const tx = await program.methods.endEvent()
+    //         .accounts({
+    //             authority: programAuthority.publicKey,
+    //             event: contest,
+    //             pythPriceFeed: priceFeed
+    //         })
+    //         .signers([programAuthority])
+    //         .rpc()
 
-            console.log(tx)
-            // remove entry from table
-        }
-    }
+    //         console.log(tx)
+    //         // remove entry from table
+    //     }
+    //}
 }
