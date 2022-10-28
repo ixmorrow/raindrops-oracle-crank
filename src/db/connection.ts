f2ddd1eec40f4c4302635b569ba7bf2966d9f246
ixmorrow <ivanmorrow14@gmail.com>
2022-10-28 10:21:04 -0500

    disable sequelize logging

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,6 +3,7 @@ const { Sequelize, DataTypes } = require('sequelize')
 export const sequelize = new Sequelize('d7h1c3hu4fa8ke', 'zyxrejtagrdmoy', 'c08dff69e57d0ae68c293de18691f594b38ec5098d39caad81890cab4658d39c', {
     host: 'ec2-3-213-66-35.compute-1.amazonaws.com',
     dialect: 'postgres',
+    logging: false,
     dialectOptions: {
         ssl: {
           require: true, // This will help you. But you will see nwe error
